c6829e3b906c410552d73419391d1c2c6f025950
Tomasz Taraś <tomasztaras@outlook.com>
2026-02-26 10:52:51 +0100

    add helper scripts

---
+++ switch_local_dev.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sed -i 's/https:\/\/ttomasz\.github\.io\/ttursm/http:\/\/localhost:8000\/web/g' ./web/index.html
+sed -i 's/https:\/\/ttomasz\.github\.io\/ttursm/http:\/\/localhost:8000\/web/g' ./web/style.css
+sed -i 's/https:\/\/ttomasz\.github\.io\/ttursm/http:\/\/localhost:8000\/web/g' ./web/styles/map_style.json
---
+++ switch_public.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+sed -i 's/http:\/\/localhost:8000\/web/https:\/\/ttomasz\.github\.io\/ttursm/g' ./web/index.html
+sed -i 's/http:\/\/localhost:8000\/web/https:\/\/ttomasz\.github\.io\/ttursm/g' ./web/style.css
+sed -i 's/http:\/\/localhost:8000\/web/https:\/\/ttomasz\.github\.io\/ttursm/g' ./web/styles/map_style.json
